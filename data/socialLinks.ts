120bdd44f0703e8d135486f4a99f1d8207cd0320
Monwar Khan <monwar259@gmail.com>
2025-06-02 13:49:29 +0100

    Created Introduction Section

---
+++ data/socialLinks.ts
@@ -0,0 +1,16 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export const socialIcons = [
+  {
+    icon: Github,
+    link: "https://github.com/MonwarK",
+  },
+  {
+    icon: Linkedin,
+    link: "https://www.linkedin.com/in/monwar-khan/",
+  },
+  {
+    icon: Mail,
+    link: "mailto:monwar259@gmail.com",
+  },
+];
